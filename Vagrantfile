37279f72b821015810bbc5feecdf69f30c9ab40e
Hisashi Morita <hisashim@workbook.org>
2014-02-18 23:09:06 +0900

    Introduce shell provisioner

--- Vagrantfile
+++ Vagrantfile
@@ -5,4 +5,8 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = ENV['BASE']
+
+  Dir.glob("shell/*").sort.each do |sh|
+    config.vm.provision :shell, path: sh
+  end
 end
---
+++ shell/00setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+## dpkg and apt
+
+SOURCES_LIST="/etc/apt/sources.list"
+# use official repository (for squid-deb-proxy)
+sed -i 's/mirrors.kernel.org/cdn.debian.net/g' $SOURCES_LIST
+# enable contrib and non-free
+sed -i 's/wheezy main$/wheezy main contrib non-free/g' $SOURCES_LIST
+# avoid interruption
+export DEBIAN_FRONTEND=noninteractive
